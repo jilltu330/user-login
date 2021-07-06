10cc1651b1cabe5fb94c083cea25f251ec6d77a5
Jill <jilltu330@gmail.com>
2021-07-06 22:51:16 +0800

    feat: login verification & error notice

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const PORT = 3000
 
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const User = require('./models/user')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true }))
 
 mongoose.connect('mongodb://localhost/user-auth', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -17,7 +20,29 @@ db.once('open', () => { console.log('mongodb connected!') })
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.render('index')
+  const errorNotice = `Email或Password錯誤`
+  console.log(req.query)
+  if (req.query.state === 'loginError') {
+    res.render('index', { errorNotice })
+  } else {
+    res.render('index')
+  }
+})
+
+//login
+app.post('/login', (req, res) => {
+  const info = req.body
+  return User.find({ email: info.email, password: info.password })
+    .lean()
+    .then((users) => {
+      if (users.length > 0) {
+        res.render('welcome', { user: users[0] })
+      } else {
+        res.redirect('/?state=loginError')
+      }
+    }
+    )
+    .catch(error => console.log(error))
 })
 
 // 設定 port
