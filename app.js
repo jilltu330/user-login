c3212545b3e88058cb15e7b92286f4fa338efd9d
Jill <jilltu330@gmail.com>
2021-07-06 11:58:58 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+const exphbs = require('express-handlebars');
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 // 設定 port
