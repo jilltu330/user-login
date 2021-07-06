36d4b3027ee7b0cb0d6cc3c5d41914807028fc50
Jill <jilltu330@gmail.com>
2021-07-06 21:03:21 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection // 取得資料庫連線狀態
+db.on('error', () => { // 連線異常
+  console.log('mongodb error!')
+})
+db.once('open', () => { // 連線成功
+  console.log('mongodb connected!')
+})
 
 // 設定首頁路由
 app.get('/', (req, res) => {
